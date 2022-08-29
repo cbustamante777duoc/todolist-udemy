75d8b9596c4401d4ffacd95a708280613d540353
cra.bustamante@alumnos.duoc.cl <Verdeamor+777>
2022-08-29 12:18:16 -0400

    app todo

--- src/components/InputTask.js
+++ src/components/InputTask.js
@@ -1,102 +1,37 @@
-import { Select, Input, Button, Grid, Header, Icon } from "semantic-ui-react";
-import { useState } from "react";
-import { v4 as uuidv4 } from "uuid";
+import { Select, Input, Grid,Button } from 'semantic-ui-react';
+// import { Select, Input, Button, Grid, Header, Icon } from "semantic-ui-react";
+
 
 const options = [
-  { key: "deporte", text: "Deporte", value: "deporte" },
-  { key: "casa", text: "Casa", value: "casa" },
-  { key: "oficina", text: "Oficina", value: "oficina" },
-  { key: "otra", text: "Otra", value: "otra" },
+    { key: "deporte", text: "Deporte", value: "deporte" },
+    { key: "casa", text: "Casa", value: "casa" },
+    { key: "oficina", text: "Oficina", value: "oficina" },
+    { key: "otra", text: "Otra", value: "otra" },
 ];
 
-export default function InputTask(props) {
-  const [task, setTask] = useState({
-    idTask: "",
-    taskName: "",
-    categoryTask: "",
-  });
-  const [error, setError] = useState(false);
+export default function InputTask() {
+    return (
+        <>
+            <Grid centered columns={2}>
+                <Input type="text" action>
+                    <Input
+                        size="small"
+                        icon="add"
+                        placeholder="Escribe tu tarea..."
+                        iconPosition="right"
+                        name="taskName"
+                    />
+                </Input>
+                <Select
+                    compact
+                    options={options}
+                    className="select-form-task"
+                    name="categoryTask"
+                    placeholder="Categoria"
+                />
+                <Button type="submit" color="violet">Añadir tarea</Button> 
+            </Grid>
+        </>
+    )
+};
 
-  const { createTask } = props;
-
-  const onChangeTask = (e) => {
-    setTask({
-      ...task,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const onChangeCategoryTask = (e, data) => {
-    setTask({
-      ...task,
-      [data.name]: data.value,
-    });
-  };
-
-  const onSubmitTask = (e) => {
-    // que no recargue la página
-    e.preventDefault();
-
-    // validación
-    if (task.taskName.trim() === "") {
-      setError(true);
-      return;
-    }
-
-    // eliminar el mensaje previo
-    setError(false);
-
-    // asignar un ID
-    task.idTask = uuidv4();
-
-    // crear la tarea
-    createTask(task);
-
-    // limpiar los inputs
-    setTask({
-      idTask: "",
-      taskName: "",
-      categoryTask: "",
-    });
-  };
-
-  return (
-    <>
-      <Grid centered columns={2}>
-        <Input type="text" action>
-          <Input
-            size="small"
-            icon="add"
-            placeholder="Escribe tu tarea..."
-            iconPosition="left"
-            name="taskName"
-            value={task.taskName}
-            onChange={onChangeTask}
-          />
-          <Select
-            compact
-            options={options}
-            className="select-form-task"
-            name="categoryTask"
-            placeholder="Categoria"
-            value={task.categoryTask}
-            onChange={onChangeCategoryTask}
-          />
-          <p>Modificando nuestro proyecto</p>
-          <Button type="submit" color="violet" onClick={onSubmitTask}>
-            Añadir tarea
-          </Button>
-        </Input>
-      </Grid>
-      {error && (
-        <Grid centered>
-          <Header as="h4" color="red" className="alert-error-form">
-            <Icon name="close" />
-            <Header.Content>La tarea es obligatoria</Header.Content>
-            <Icon name="close" />
-          </Header>
-        </Grid>
-      )}
-    </>
-  );
-}
