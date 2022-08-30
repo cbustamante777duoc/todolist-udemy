cf9f11502429f7e66bf2c7099a780eb890e5e847
cra.bustamante@alumnos.duoc.cl <Verdeamor+777>
2022-08-30 11:46:19 -0400

    validaciones formulario

--- src/components/InputTask.js
+++ src/components/InputTask.js
@@ -1,6 +1,7 @@
-import { Select, Input, Grid,Button } from 'semantic-ui-react';
+import { Select, Input, Grid, Button, Header, Icon } from 'semantic-ui-react';
 // import { Select, Input, Button, Grid, Header, Icon } from "semantic-ui-react";
-
+import { useState } from 'react';
+import { v4 as uuidv4 } from "uuid";
 
 const options = [
     { key: "deporte", text: "Deporte", value: "deporte" },
@@ -10,6 +11,64 @@ const options = [
 ];
 
 export default function InputTask() {
+    const [task, setTask] = useState({
+        idTask: "",
+        taskName: "",
+        categoryTask: "",
+    });
+    const [error, setError] = useState(false);
+
+    /**
+     * captura lo que viene en el input taskName
+     * @param {e} evento por teclado 
+     */
+    //[e.target.name] = Input name="taskName"
+    const onChangeTask = (e) => {
+        setTask({
+            ...task,
+            [e.target.name]: e.target.value//actualiza
+        });
+        console.log(task);
+    }
+
+    /**
+     * recupera el valor del select categoryTask
+     * @param {*} e 
+     * @param {*} data recupera los valores que estan en el select 
+     */
+    const onChangeCategory = (e, data) => {
+        setTask({
+            ...task,
+            [data.name]: data.value//actualiza
+        });
+
+        console.log(task);
+
+
+    }
+
+    const onSubmitTask = (e) => {
+    
+        //evitar recargar la pagina
+        e.preventDefault();
+        
+        //validaciones
+        if (task.taskName.trim() ==="") {
+            setError(true);
+            return;
+        }
+
+        //eliminar mensajes de error
+        setError(false);
+
+        //asignar id
+        task.idTask = uuidv4();
+        console.log(`id ${task.idTask}`);
+
+
+    }
+
+
     return (
         <>
             <Grid centered columns={2}>
@@ -18,8 +77,10 @@ export default function InputTask() {
                         size="small"
                         icon="add"
                         placeholder="Escribe tu tarea..."
-                        iconPosition="right"
+                        iconPosition="left"
                         name="taskName"
+                        value={task.taskName}
+                        onChange={onChangeTask}
                     />
                 </Input>
                 <Select
@@ -28,9 +89,27 @@ export default function InputTask() {
                     className="select-form-task"
                     name="categoryTask"
                     placeholder="Categoria"
+                    value={task.categoryName}
+                    onChange={onChangeCategory}
                 />
-                <Button type="submit" color="violet">Añadir tarea</Button> 
+                <Button
+                    type="submit"
+                    color="violet"
+                    onClick={onSubmitTask}>Añadir tarea
+                </Button>
             </Grid>
+            {error && (
+                <Grid centered>
+                    <Header
+                        as="h4"
+                        color="red"
+                        className="alert-error-form">
+                        <Icon name="close" />
+                        <Header.Content>La tares es obligatoria </Header.Content>
+                        <Icon name="close" />
+                    </Header>
+                </Grid>
+            )}
         </>
     )
 };
